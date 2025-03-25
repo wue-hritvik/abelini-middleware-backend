4970cee9a6fd1821a9bb6180dbdab81cd727310f
Hritvik Soni <hritviksoni768@gmail.com>
2025-03-25 18:01:32 +0530

    solution for cors issue swagger 3

--- src/main/java/com/abelini_middleware_backend/SwaggerConfig.java
+++ src/main/java/com/abelini_middleware_backend/SwaggerConfig.java
@@ -1,16 +1,16 @@
-//package com.abelini_middleware_backend;
-//
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.servers.Server;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class SwaggerConfig {
-//
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        return new OpenAPI()
-//                .addServersItem(new Server().url("https://api.abelane.com").description("Production Server"));
-//    }
-//}
+package com.abelini_middleware_backend;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("https://api.abelane.com/abelini").description("Production Server"));
+    }
+}
